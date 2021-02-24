0dfe35ca1faaf312677337a7c73c82fc6ae71213
kawaban <64012749+kawaban@users.noreply.github.com>
2021-02-24 13:03:55 +0500

    Create products.ts

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,103 @@
+eexport const products = [
+  {
+    id: 1,
+    image: 'https://m.media-amazon.com/images/I/71Oq+h-mTmL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'Amazon Essentials Women\'s Surplice Dress',
+    price: 20.00,
+    description: '"Personal Shopper Style Tip: Style this dress with dainty layered necklaces and a straw hat for any sunny day" - Emmely',
+    rating: '4.5 stars',
+    link:'https://www.amazon.com/Amazon-Essentials-Womens-Solid-Surplice/dp/B07JXVH52N/ref=sr_1_25?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-25'
+  },
+  {
+    id: 2,
+    image: 'https://m.media-amazon.com/images/I/61yNv68i2aL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'AlvaQ Women Summer Spaghetti Strap Button Down V Neck Sleeveless Casual Mini Dress',
+    price: 9.99,
+    description: 'Summer Floral Print Button Down V Neck Sleeveless Swing Tank Dress For Women',
+    rating: '4 stars',
+    link: 'https://www.amazon.com/AlvaQ-Summer-Spaghetti-Button-Skater/dp/B07PRJT8Y2/ref=sr_1_48?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-48'
+  },
+  {
+    id: 3,
+    image: 'https://m.media-amazon.com/images/I/41dIdwZ9GFL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'BORIFLORS Women\'s Sexy Basic Long Sleeve Turtleneck Bodycon Party Long Pencil Dress',
+    price: 16.99,
+    description: 'Design: This basic bodycon midi dress designs in long sleeve, high turtleneck,simple,classic,brushed lining make it very warm in spring, fall and winter, below knee length, long tight fitted like a glove, very flattering and classy for women, making you get tons of compliments',
+    rating: '4.5 stars',
+    link: 'https://www.amazon.com/BORIFLORS-Womens-Sleeve-Turtleneck-Bodycon/dp/B0821K76PY/ref=sr_1_46?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-46'
+  },
+  {
+    id: 4,
+    image:'https://m.media-amazon.com/images/I/51IlE+vzK-L._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'VIISHOW Women\'s Long Sleeve Loose Plain Empire Waist Maxi Dresses Casual Long Dresses with Pockets',
+    price: 15.07,
+    description: 'Material:95% Rayon,5% Spandex,Note 1:The WHITE color is a little bit see through, pairing a nude,lighter color underwear or a slip with it would be better. Note 2: The Pure "White" dress is a little heavier than the other colors.Note 3: The Floral Print is random,the floral print on each dress may be different,but they are definitely made of same pattern fabric.Please pay attention to the above points before placing an order, thanks!',
+    rating: '4.5 stars',
+    link: 'https://www.amazon.com/Viishow-Womens-Sleeve-Dresses-Pockets/dp/B07JW1H1J1/ref=sr_1_34?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-34'
+  },
+  {
+    id: 5,
+    image: 'https://m.media-amazon.com/images/I/815ghzVp7OL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'Milumia Women\s Floral Embroidery Mesh Round Neck Tunic Party Dress',
+    price: 26.99,
+    description: 'Feature: Round Neck, Long Sleeve, Short Mini Dress, Cocktail Dress, Embroidery, Contrast Mesh, Black dress, Floral Dress, Party Dress, Overlay Dress',
+    rating: '4 stars',
+    link: 'https://www.amazon.com/Milumia-Womens-Floral-Embroidery-Sheer/dp/B07QYSZBGX/ref=sr_1_18?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-18'
+  },
+  {
+    id: 6,
+    image: 'https://m.media-amazon.com/images/I/61Cu7jVxdCL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'PRETTYGARDEN Women’s Elegant Long Lantern Sleeve Short Dress Crewneck Tie Waist Knit Cocktail Dress',
+    price: 26.99,
+    description: 'Unique lantern sleeves and wide belt design can hide the belly and arms, show your great slim fit and good temperament',
+    rating: '4 stars',
+    link: 'https://www.amazon.com/PRETTYGARDEN-Womens-Elegant-Lantern-Crewneck/dp/B07FR9F2G1/ref=sr_1_8?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-8'
+  },
+  {
+    id: 7,
+    image: 'https://m.media-amazon.com/images/I/61yTwrWRAmL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'BerryGo Women\s Embroidery Pearl Button Down Dress V Neck Spaghetti Strap Maxi Dress',
+    price: 29.49,
+    description: 'This beach sundress will guarantee your next kiss underneath the moonlight. Beautiful round pearls button-down front, hollow out waist and dress bottom design, embroidery featured flare wedding guest dress.',
+    rating: '4.5 stars',
+    link: 'https://www.amazon.com/BerryGo-Womens-Embroidery-Spaghetti-White-M/dp/B07M9QS6NW/ref=sr_1_9?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-9'
+  },
+  {
+    id: 8,
+    image: 'https://m.media-amazon.com/images/I/71Qsk7H-ycL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'Floerns Women\'s V Back Inslace Layered Ruffle Hem Flutter Sleeve Dress',
+    price: 31.99,
+    description: 'Fit and flare silhouette ruffle short dresses with zipper',
+    rating: '4 stars',
+    link: 'https://www.amazon.com/Floerns-Womens-Inslace-Layered-Flutter/dp/B07Q4BYCPC/ref=sr_1_35?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-35'
+  },
+  {
+    id: 9,
+    image: 'https://m.media-amazon.com/images/I/51s6v+IKCkL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'Romacci Women Boho Dress Casual Irregular Maxi Dresses Layer Vintage Loose Long Sleeve Linen Dress with Pockets',
+    price: 20.99,
+    description: 'The casual dress is cut and made with comfortable soft fabrics, and the smooth lines can give a beautiful figure shape.',
+    rating: '4.5 stars',
+    link: 'https://www.amazon.com/Romacci-Irregular-Dresses-Vintage-Pockets/dp/B076JB54QQ/ref=sr_1_38?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-38'
+  },
+  {
+    id: 10,
+    image: 'https://m.media-amazon.com/images/I/81Cw4LTy+aL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'Romwe Women\'s Casual Floral Print Off Shoulder Trumpet Sleeve Swing Dress',
+    price: 26.99,
+    description: 'Sexy Slash Neck,Trumpet Sleeves,Elastic High Waist.',
+    rating: '4 stars',
+    link: 'https://www.amazon.com/Romwe-Womens-Casual-Shoulder-Trumpet/dp/B07L9SK6ZZ/ref=sr_1_21?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-21'
+  },
+  {
+    id: 11,
+    image: 'https://m.media-amazon.com/images/I/7106bvimmJL._MCnd_SEARCH281250_AC_UL640_QL65_.jpg',
+    name: 'Relipop Summer Women Short Sleeve Print Dress V Neck Casual Short Dresses',
+    price: 20.99,
+    description: ' Relipop is a registered trademark. We are committed to providing high quality fashion clothing to make your life better. Choose Relipop, choose real popularity!',
+    rating: '4 stars',
+    link: 'https://www.amazon.com/Relipop-Summer-Sleeve-Casual-Dresses/dp/B07DPDSMCQ/ref=sr_1_7?dchild=1&pf_rd_i=16225018011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e9a7a2cd-d373-460c-8c25-702b5e2acb03&pf_rd_r=YBDD83JJ6145ZBQJ44BN&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614093551&rnid=1040660&s=fashion-womens-intl-ship&sr=1-7'
+  }
+
+
+];
