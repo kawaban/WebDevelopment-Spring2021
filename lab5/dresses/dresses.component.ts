6c071febdc0ffed2e3f02a54a82a1d1865409aae
kawaban <64012749+kawaban@users.noreply.github.com>
2021-03-03 02:45:47 +0500

    Add files via upload

---
+++ lab5/dresses/dresses.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { products } from '../products';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-dresses',
+  templateUrl: './dresses.component.html',
+  styleUrls: ['./dresses.component.css']
+})
+export class DressesComponent implements OnInit {
+  product: any;
+  products = products;
+  likes = 0;
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+  likeProduct(){
+    this.likes++;
+  }
+}
