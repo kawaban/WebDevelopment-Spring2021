9218b9f9dd4d12d0bcff5bd5799838b9d0b59d27
kawaban <64012749+kawaban@users.noreply.github.com>
2021-03-03 02:45:01 +0500

    Add files via upload

---
+++ lab5/categories/categories.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { categories} from '../categories';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+  categories = categories;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
